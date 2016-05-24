a7f39402db68fd7a0237c54d50f9d29586629ad9
willriggins <williamriggins2013@gmail.com>
2016-05-24 09:53:21 -0400

    refactor

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -36,7 +36,7 @@ public class Main {
             if (toDoItem.isDone) {
                 checkbox = "[x]";
             }
-            System.out.println(checkbox + " " + i + ". " + toDoItem.text);
+            System.out.printf("%s %s. %s\n", checkbox, i, toDoItem.text);
             i++;
         }
 
